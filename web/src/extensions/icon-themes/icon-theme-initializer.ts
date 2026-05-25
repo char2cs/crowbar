3c4b4545bffb4b714304b4c02cf4c56915a6b8ff
Mateo Urrutia <me@char2cs.net>
2026-05-25 20:06:33 -0300

    fix(icon-themes): register built-in themes with source metadata

--- web/src/extensions/icon-themes/icon-theme-initializer.ts
+++ web/src/extensions/icon-themes/icon-theme-initializer.ts
@@ -1,4 +1,5 @@
 import { iconThemeRegistry } from "./icon-theme-registry"
+import type { IconThemeSource } from "./types"
 import { classicIconTheme } from "./builtin/classic-theme"
 import { materialIconTheme } from "./builtin/material-theme"
 import { colorfulMaterialIconTheme } from "./builtin/colorful-material-theme"
@@ -6,11 +7,13 @@ import { compactIconTheme } from "./builtin/compact-theme"
 import { minimalIconTheme } from "./builtin/minimal-theme"
 import { noneIconTheme } from "./builtin/none-theme"
 
+const BUILTIN_SOURCE: IconThemeSource = { extensionId: "builtin", isBundled: true }
+
 export function initializeIconThemes() {
-  iconThemeRegistry.registerTheme(classicIconTheme)
-  iconThemeRegistry.registerTheme(materialIconTheme)
-  iconThemeRegistry.registerTheme(colorfulMaterialIconTheme)
-  iconThemeRegistry.registerTheme(compactIconTheme)
-  iconThemeRegistry.registerTheme(minimalIconTheme)
-  iconThemeRegistry.registerTheme(noneIconTheme)
+  iconThemeRegistry.registerTheme(classicIconTheme, BUILTIN_SOURCE)
+  iconThemeRegistry.registerTheme(materialIconTheme, BUILTIN_SOURCE)
+  iconThemeRegistry.registerTheme(colorfulMaterialIconTheme, BUILTIN_SOURCE)
+  iconThemeRegistry.registerTheme(compactIconTheme, BUILTIN_SOURCE)
+  iconThemeRegistry.registerTheme(minimalIconTheme, BUILTIN_SOURCE)
+  iconThemeRegistry.registerTheme(noneIconTheme, BUILTIN_SOURCE)
 }
