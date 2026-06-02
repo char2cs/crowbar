a72b1bbae179564676a4d855700d9ed7c6b98659
Mateo Urrutia <me@char2cs.net>
2026-06-02 17:39:01 -0300

    revert(git): restore git-panel to TabsList/TabsTab implementation

--- web/src/features/git/components/git-panel.tsx
+++ web/src/features/git/components/git-panel.tsx
@@ -12,10 +12,10 @@ export function GitPanel() {
 
   return (
     <Tabs defaultValue="changes" className="flex flex-1 flex-col overflow-hidden">
-      <div className="flex shrink-0 items-center px-2 py-1.5">
-        <TabsList variant="default" className="w-full">
-          <TabsTab value="changes" className="flex-1 justify-center">Changes</TabsTab>
-          <TabsTab value="history" className="flex-1 justify-center">History</TabsTab>
+      <div className="flex shrink-0 items-center px-3 py-1.5">
+        <TabsList variant="default">
+          <TabsTab value="changes">Changes</TabsTab>
+          <TabsTab value="history">History</TabsTab>
         </TabsList>
       </div>
 
