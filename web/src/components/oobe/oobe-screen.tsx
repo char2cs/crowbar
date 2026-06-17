5b087b6681971daaa296e94369aae5b00990410a
Mateo Urrutia <me@char2cs.net>
2026-06-17 10:58:36 -0300

    feat(oobe): add ShaderGradient animated background to OOBE screen

--- web/src/components/oobe/oobe-screen.tsx
+++ web/src/components/oobe/oobe-screen.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from '@tanstack/react-router'
+import { ShaderGradient, ShaderGradientCanvas } from '@shadergradient/react'
 import {
   Empty,
   EmptyMedia,
@@ -25,8 +26,48 @@ export function OobeScreen() {
   }
 
   return (
-    <div className="flex h-screen flex-col bg-background">
-      <Empty>
+    <div className="relative flex h-screen flex-col overflow-hidden">
+      {/* Animated gradient background */}
+      <div className="pointer-events-none absolute inset-0">
+        <ShaderGradientCanvas style={{ width: '100%', height: '100%' }} fov={45} pixelDensity={1}>
+          <ShaderGradient
+            animate="on"
+            brightness={0.8}
+            cAzimuthAngle={270}
+            cDistance={0.5}
+            cPolarAngle={180}
+            cameraZoom={15.09}
+            color1="#73bfc4"
+            color2="#ff810a"
+            color3="#8da0ce"
+            envPreset="city"
+            grain="on"
+            lightType="env"
+            positionX={-0.1}
+            positionY={0}
+            positionZ={0}
+            range="disabled"
+            rangeEnd={40}
+            rangeStart={0}
+            reflection={0.4}
+            rotationX={0}
+            rotationY={130}
+            rotationZ={70}
+            shader="defaults"
+            type="sphere"
+            uAmplitude={3.2}
+            uDensity={0.8}
+            uFrequency={5.5}
+            uSpeed={0.3}
+            uStrength={0.3}
+            uTime={0}
+            wireframe={false}
+          />
+        </ShaderGradientCanvas>
+      </div>
+
+      {/* Content */}
+      <Empty className="relative z-10">
         <EmptyMedia variant="icon" className="size-20">
           <CrowbarIcon className="size-10 text-foreground" />
         </EmptyMedia>
